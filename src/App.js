577dd28e75b378da3c3e6ba01ec1d5d0de197d2e
Bhon Bunnag <bhonris@gmail.com>
2021-08-03 21:11:21 -0700

    test1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
@@ -7,14 +8,21 @@ const SERVER_ADDRESS = 'http://localhost:3003'
 
 function App() {
   const [fruit, setFruit] = useState('');
+  // 4. use hook to establish stateful variable for maxPrice
+
 
   async function getFruit(){
     const receivedFruit = await axios({
       method: 'get',
       url: `${SERVER_ADDRESS}/fruit/${fruit}`,
     })
-    console.log(receivedFruit)
-    alert(receivedFruit);
+    alert(`Fruit: ${receivedFruit.name}, Price: ${receivedFruit.price}`)
+  }
+
+  // 5. complete this function (very similar to getFruit)
+  // Using url: `${SERVER_ADDRESS}/price/${maxPrice}`
+  async function getListFromMaxPrice(){
+    return false
   }
 
   return (
@@ -22,6 +30,7 @@ function App() {
        <label for="fname">Get Fruit!</label> <br/>
        <input value={fruit} onChange={e => setFruit(e.target.value)} />
       <button onClick={getFruit}> Submit </button>
+      {/* 3. Create elements to input max price */}
     </div>
   );
 }
